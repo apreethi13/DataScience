f94ee5bfeccda950580f93858c0cdd4f81f64076
Preethi-Anantharaman1 <codeurlife2100@gmail.com>
2020-06-19 19:05:09 -0500

    First Commit.. getting started with Data Science Basics.

---
+++ basics/type_conversion.py
@@ -0,0 +1,17 @@
+# Simple Python program that showcases type conversion
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+# Print Pi_float
+print(str(pi_float) + ' is a ' + str(type(pi_float)))
